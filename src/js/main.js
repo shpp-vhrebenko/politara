dbfe3eda0829a04c59d0fe43c166ac10411cbeac
Alex <big.dev.mail@gmail.com>
2017-06-29 16:49:30 +0300

    page contacts done

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,21 @@
 //= custom/custom.js
 
 
+$(document).ready(function(){
+  /*$('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });*/
+  /*$('.dropdown-submenu a.submenu-toggle').hover(function(){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });*/
 
+  /*$('ul.nav li.dropdown').hover(function() {
+	  $(this).find('.dropdown-menu.parent').stop(true, true).delay(200).fadeIn(500);
+	}, function() {
+	  $(this).find('.dropdown-menu.parent').stop(true, true).delay(200).fadeOut(500);
+	});*/
+});
