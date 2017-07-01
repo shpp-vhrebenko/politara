2a3433e17388da42f1512fa6577e709d6747b270
shpp-vhrebenko <vhrebenko@gmail.com>
2017-07-01 18:22:34 +0300

    fix adaptive

--- build/js/main.js
+++ build/js/main.js
@@ -15,6 +15,12 @@ $(function() {
 
 
 $(document).ready(function(){
+ /* $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+    event.preventDefault(); 
+    event.stopPropagation(); 
+    $(this).parent().siblings().removeClass('open');
+    $(this).parent().toggleClass('open');
+  });*/
   /*$('.dropdown-submenu a.test').on("click", function(e){
     $(this).next('ul').toggle();
     e.stopPropagation();
--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,12 @@
 
 
 $(document).ready(function(){
+ /* $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+    event.preventDefault(); 
+    event.stopPropagation(); 
+    $(this).parent().siblings().removeClass('open');
+    $(this).parent().toggleClass('open');
+  });*/
   /*$('.dropdown-submenu a.test').on("click", function(e){
     $(this).next('ul').toggle();
     e.stopPropagation();
